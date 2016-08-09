a3f8ed458e61bcd1eb32b322faf44eb535e29f42
Phillip Lorenzo <pllearns@gmail.com>
2016-08-09 14:12:28 -0700

    removed comments from add.js

--- dist/add.js
+++ dist/add.js
@@ -8,9 +8,3 @@ var mathAdd = function mathAdd(num1, num2) {
 };
 
 exports.mathAdd = mathAdd;
-
-//
-// let x = number1
-// let y = number2
-// if (x + y) {
-//   return sum(number1 + number 2)
--- src/add.js
+++ src/add.js
@@ -3,9 +3,3 @@ const mathAdd = (num1, num2) => {
 }
 
 export { mathAdd }
-
-//
-// let x = number1
-// let y = number2
-// if (x + y) {
-//   return sum(number1 + number 2)
