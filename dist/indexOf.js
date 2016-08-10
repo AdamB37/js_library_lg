2562c30b9ec83e153d0045f6b1b7594a2268e747
James D Stewart Jr <jamstewjr@gmail.com>
2016-08-10 15:34:11 -0700

    indexOf is now refactored.

--- dist/indexOf.js
+++ dist/indexOf.js
@@ -1,14 +1,23 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-var indexOf = function indexOf(arr, value, start) {
-  for (var i = start; i < arr.length; i++) {
-    if (arr[i] === value) {
-      return i;
-    } else return -1;
+var indexOf = function indexOf(arr, value) {
+  var start = arguments.length <= 2 || arguments[2] === undefined ? 0 : arguments[2];
+
+  if (!(arr instanceof Array)) {
+    return -1;
   }
+
+  if (typeof arr == 'array') {
+    for (var i = start; i < arr.length; i++) {
+      if (arr[i] === value) {
+        return i;
+      }
+    }
+  }
+  return -1;
 };
 
 exports.indexOf = indexOf;
--- src/indexOf.js
+++ src/indexOf.js
@@ -1,11 +1,16 @@
-const indexOf = (arr, value, start) => {
-  for (var i = start; i < arr.length; i++) {
-    if (arr[i] === value){
-      return i
-    }else
-      return -1
+const indexOf = (arr, value, start=0) => {
+  if( ! (arr instanceof Array) ) {
+    return -1
   }
-}
 
+  if (typeof arr == 'array'){
+    for (var i = start; i < arr.length; i++) {
+      if (arr[i] === value) {
+        return i
+      }
+    }
+  }
+  return -1
+}
 
 export { indexOf }
