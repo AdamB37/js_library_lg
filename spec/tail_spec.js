ed9ec340e356209144b13088401329c1c7713ab2
Phillip Lorenzo <pllearns@gmail.com>
2016-08-11 10:22:26 -0700

    fixed spec formatting

--- spec/tail_spec.js
+++ spec/tail_spec.js
@@ -1,11 +1,11 @@
 import { tail } from '../src/tail'
 
-fdescribe( 'tail', () => {
+describe( 'tail', () => {
   it( 'Gets all but the first element of array.', () => {
-  expect(tail( [1, 2, 3, 4] )).toEqual( [2, 3, 4] )
+    expect(tail( [1, 2, 3, 4] )).toEqual( [2, 3, 4] )
   })
 
   it( 'Gets all but the first element of array.', () => {
-  expect(tail( [5, 5, 4, 4] )).toEqual( [5, 4, 4] )
+    expect(tail( [5, 5, 4, 4] )).toEqual( [5, 4, 4] )
   })
 })
