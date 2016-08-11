3d3eb10703174a1b6ce7714648899dc42af9e9a5
James D Stewart Jr <jamstewjr@gmail.com>
2016-08-10 17:43:23 -0700

    adding readibility to the code.

--- dist/size.js
+++ dist/size.js
@@ -1,13 +1,15 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+    value: true
 });
 var size = function size(obj) {
-  var iterate = 0;
-  for (var props in obj) {
-    iterate++;
-  }
-  return iterate;
+    var iterate = 0;
+
+    for (var props in obj) {
+        iterate++;
+    }
+
+    return iterate;
 };
 exports.size = size;
--- src/size.js
+++ src/size.js
@@ -1,9 +1,11 @@
 const size = (obj) => {
-      var iterate = 0
+    var iterate = 0
+
     for (var props in obj){
       iterate ++
     }
-      return iterate
+
+    return iterate
 
 }
 export { size }
