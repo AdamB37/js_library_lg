c83899123a7c3091bc1ffddcd4240daf77b4b65e
James D Stewart Jr <jamstewjr@gmail.com>
2016-08-11 15:20:31 -0700

    take function is created.

---
+++ dist/take.js
@@ -0,0 +1,22 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var take = function take(array) {
+  var size = arguments.length <= 1 || arguments[1] === undefined ? 1 : arguments[1];
+
+  if (!(array instanceof Array)) {
+    return [];
+  }
+
+  var result = [];
+
+  for (var index = 0; index < size; index++) {
+    result.push(array[index]);
+  }
+
+  return result;
+};
+
+exports.take = take;
---
+++ spec/take_spec.js
@@ -0,0 +1,15 @@
+import { take } from '../src/take'
+
+describe( 'take', () => {
+  it( 'uses a default size of 1', () => {
+    expect( take( [ 1, 2, 3] ) ).toEqual( [ 1 ] )
+  })
+
+  it( 'returns an array of specified size from start of array', () => {
+    expect( take( [ 1, 2, 3, 4 ], 2 ) ).toEqual( [1, 2] )
+  })
+
+  it( 'returns an empty array for non-array input', () => {
+    expect( take( null, 2 ) ).toEqual( [] )
+  })
+})
