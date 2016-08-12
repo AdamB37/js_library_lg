6cc968d9681bc7b9cf6624d2e5acdb3bad09fe73
James D Stewart Jr <jamstewjr@gmail.com>
2016-08-12 11:26:49 -0700

    tests corrections.

--- spec/nth_spec.js
+++ spec/nth_spec.js
@@ -6,13 +6,12 @@ describe( 'nth', () => {
     expect(nth([1, 2, 1, 4], 2)).toEqual(1)
   })
 
-  it( 'returns the index value - the input from the end of the array, if input is negative', () => {
-    expect(nth([1, 2, 1, 4], -2)).toEqual(1)
-  })
-
   it( 'returns undefined, if input is out of range of input array', () => {
     expect(nth([1, 2, 1, 4], 8)).toEqual(undefined)
   })
 
+  it( 'returns the index value - the input from the end of the array, if input is negative', () => {
+    expect(nth([1, 2, 1, 4], -2)).toEqual(1)
+  })
 
 })
